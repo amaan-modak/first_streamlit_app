c2d37763478908c588c30fbf25d7f0106328315d
Amaan Modak <22226503+amaan-modak@users.noreply.github.com>
2023-09-15 11:01:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,9 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach Smoothie')
 streamlit.text('🐔Hard-boiled Egg')
 
+
+streamlit.header('Build your own first smoothie')
+
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
