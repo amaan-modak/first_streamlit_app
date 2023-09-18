f8989f71a98f8b420f5f03e5f11ccf8b37957415
Amaan Modak <22226503+amaan-modak@users.noreply.github.com>
2023-09-18 12:27:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Outputs data as a table
 streamlit.dataframe(fruityvice_normalized)
 
+#Adding a breakpoint
+streamlit.stop()
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
